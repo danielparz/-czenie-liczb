16bd1bc2e574f3a9b5ca7cdf016f04bb191cb538
danielparz <dparzynski@gmail.com>
2020-06-03 08:02:49 +0200

    Dodaj metody generujące minimalną i maksymalna wartość.

--- NumberGenerator.cs
+++ NumberGenerator.cs
@@ -14,6 +14,40 @@ namespace Łączenie_liczb
             _numList.Sort(new NumbersComparator());
         }
 
+        private bool CheckNumVal(uint num, uint lastNum)
+        {
+            if(num < lastNum) return false;
+            if(num <= UInt32.MaxValue) return true;
+        }
 
+
+        public uint GetMinVal()
+        {
+            string minVal = "";
+            uint lastNum;
+            foreach(var num in _numList)
+            {
+                if(minVal == "") lastNum = 0;
+                minVal += num.ToString();
+
+                if(!CheckNumVal((uint)minVal, lastNum)) throw new Exception("Liczba minimalna utworzona z podanego ciągu nie mieści się w typie uint.");
+            }
+            return (uint)minVal;
+        }
+
+        public uint GetMaxVal()
+        {
+            string maxVal = "";
+            uint lastNum;
+
+            for(int i = _numList.Count; i > 0; i--)
+            {
+                if(maxVal == "") lastNum = 0;
+                maxVal += _numList[i].ToString();
+
+                if(!CheckNumVal((uint)minVal, lastNum)) throw new Exception("Liczba maksymalna utworzona z podanego ciągu nie mieści się w typie uint.");
+            }
+            return (uint)maxVal;            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace Łączenie_liczb
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint[] numTab = new uint[args[0]];
+            Console.WriteLine("Podano następujące dane wejściowe:");
+            foreach(var num in numTab)
+                Console.Write($" {num},");
+            Console.WriteLine();
+
+            try
+            {
+                foreach(var num in numTab)                
+                    for(int i = 0; i < num.Length; i++)                    
+                        if(!num.IsDigit(i)) throw new Exception("Podane dane wejściowe nie zawierają tylko liczb naturalnych");                                    
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            NumberGenerator numGen = new NumberGenerator(numTab);
+
+            try
+            {
+                Console.WriteLine($"Otrzymana minimalna wartość: {numGen.GetMinVal()}");
+                Console.WriteLine($"Otrzymana maksymalna wartość: {numGen.GetMaxVal()}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
